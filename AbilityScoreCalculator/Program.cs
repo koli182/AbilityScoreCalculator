2dee157e8c9c897976358c0a47dd2f415546ce0e
Oliver Kirkup <koli182@protonmail.com>
2021-08-03 16:35:10 -0400

    Add project files.

---
+++ AbilityScoreCalculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AbilityScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by: ");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount: ");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum: ");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Your Calculated Ability Score Is: " + calculator.Score);
+                Console.WriteLine("Please press q to exit the program, or any other key to continue.");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+
+            }
+        }
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("     using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("     using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
